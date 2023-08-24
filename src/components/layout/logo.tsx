3093f2cafd246617a66ebb7d73513712ab65cefc
Alpfreda <alpfreda@gmail.com>
2023-08-25 00:28:28 +0300

    Add icons

---
+++ src/components/layout/logo.tsx
@@ -0,0 +1,12 @@
+import Dots from '../dots'
+
+const Logo: React.FC = () => {
+  return (
+    <div className='logo'>
+      <Dots />
+      <span className='name'>Mehdi Akbarzadeh</span>
+    </div>
+  )
+}
+
+export default Logo
