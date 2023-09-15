580267c9ad1bd0a6a4c2c67998d75d534385dea1
Alpfreda <alpfreda@gmail.com>
2023-09-15 22:45:53 +0300

    Change configs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images : {
+    domains : ['firebasestorage.googleapis.com']
+  },
+  experimental: {
+    serverActions: true,
+  },
+  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
+  webpack: (config) => {
+    config.module.rules.push(
+      {
+        test: /\.md$/,
+        use: 'raw-loader'
+      }
+    )
+
+    return config
+  },
+}
 
 module.exports = nextConfig
