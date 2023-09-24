3e0dc2b436d821a78837ed89f7def04397546f6e
Alpfreda <alpfreda@gmail.com>
2023-09-25 00:10:13 +0300

    Allow all domains next images

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['firebasestorage.googleapis.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
   experimental: {
-    serverActions: true
+    serverActions: true,
   },
   pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
   webpack: config => {
